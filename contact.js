64ba4ee7911af368267868d3a51604955053b83f
E-Hammond <emmanuel.hammond@amalitech.org>
2021-04-20 15:17:30 +0000

    Modified the style sheet

--- contact.js
+++ contact.js
@@ -1,1 +1,16 @@
 
+
+const userName = document.getElementById("Username")
+const email = document.getElementById("email")
+const submit = document.getElementById("submit")
+
+
+submit.addEventListener("click", function(e){
+    
+        alert(`Your username is ${userName.value}
+Your email is ${email.value}
+`)
+console.log(typeof userName.value)
+e.preventDefault();
+    
+})
